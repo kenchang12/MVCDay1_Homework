6d70454c8721683f545c3f8d4d3abc32f631ca6e
kenchang12 <kenchang8412@gmail.com>
2016-04-15 18:29:45 +0800

    已把字串轉DateTime直接改成new一個DateTime

--- MVCDay1_Homework_MyAccountingBook/MVCDay1_Homework_MyAccountingBook/Controllers/HomeController.cs
+++ MVCDay1_Homework_MyAccountingBook/MVCDay1_Homework_MyAccountingBook/Controllers/HomeController.cs
@@ -40,21 +40,24 @@ namespace MVCDay1_Homework_MyAccountingBook.Controllers
             {
                 Category = "支出",
                 Money = 300,
-                ImplementDate = DateTime.ParseExact("20160101", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
+                //ImplementDate = DateTime.ParseExact("20160101", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
+                ImplementDate = new DateTime(2016, 1, 1)
             });
 
             record.Add(new IncomeAndPayRecordModel
             {
                 Category = "支出",
                 Money = 16000,
-                ImplementDate = DateTime.ParseExact("20160102", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
+                //ImplementDate = DateTime.ParseExact("20160102", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
+                ImplementDate = new DateTime(2016, 1, 2)
             });
 
             record.Add(new IncomeAndPayRecordModel
             {
                 Category = "支出",
                 Money = 8000,
-                ImplementDate = DateTime.ParseExact("20160103", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
+                //ImplementDate = DateTime.ParseExact("20160103", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
+                ImplementDate = new DateTime(2016, 1, 3)
             });
 
             return View(record);
