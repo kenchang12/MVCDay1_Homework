e3b317923a0489e022957d522d7563da10189e51
kenchang12 <kenchang8412@gmail.com>
2016-04-18 15:57:22 +0800

    在我的記賬本版面已改成MontyTempplat，且資料來源已改成localDB。

--- MVCDay1_Homework_MyAccountingBook/MVCDay1_Homework_MyAccountingBook/Controllers/HomeController.cs
+++ MVCDay1_Homework_MyAccountingBook/MVCDay1_Homework_MyAccountingBook/Controllers/HomeController.cs
@@ -7,10 +7,15 @@ using System.Web.Mvc;
 //須引入才能用IncomeAndPayRecordModel
 using MVCDay1_Homework_MyAccountingBook.Models.ViewModels;
 
+using MVCDay1_Homework_MyAccountingBook.Models;
+
 namespace MVCDay1_Homework_MyAccountingBook.Controllers
 {
     public class HomeController : Controller
     {
+
+        SkillTreeHomeworkEntities dbContext = new SkillTreeHomeworkEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -34,33 +39,61 @@ namespace MVCDay1_Homework_MyAccountingBook.Controllers
         [ChildActionOnly]
         public ActionResult IncomeAndPayList()
         {
-            List<IncomeAndPayRecordModel> record = new List<IncomeAndPayRecordModel>();
+            //List<IncomeAndPayRecordModel> record = new List<IncomeAndPayRecordModel>();
 
-            record.Add(new IncomeAndPayRecordModel
+            //record.Add(new IncomeAndPayRecordModel
+            //{
+            //    Category = "支出",
+            //    Money = 300,
+            //    //ImplementDate = DateTime.ParseExact("20160101", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
+            //    ImplementDate = new DateTime(2016, 1, 1)
+            //});
+
+            //record.Add(new IncomeAndPayRecordModel
+            //{
+            //    Category = "支出",
+            //    Money = 16000,
+            //    //ImplementDate = DateTime.ParseExact("20160102", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
+            //    ImplementDate = new DateTime(2016, 1, 2)
+            //});
+
+            //record.Add(new IncomeAndPayRecordModel
+            //{
+            //    Category = "支出",
+            //    Money = 8000,
+            //    //ImplementDate = DateTime.ParseExact("20160103", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
+            //    ImplementDate = new DateTime(2016, 1, 3)
+            //});
+
+            List<IncomeAndPayRecordModel> viewModelList = new List<IncomeAndPayRecordModel>();
+
+            var myAccounting = dbContext.AccountBook
+                                        .Take(10)
+                                        .OrderByDescending(x => x.Dateee)
+                                        //.AsEnumerable()
+                                        .ToList();
+            foreach (var item in myAccounting)
             {
-                Category = "支出",
-                Money = 300,
-                //ImplementDate = DateTime.ParseExact("20160101", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
-                ImplementDate = new DateTime(2016, 1, 1)
-            });
-
-            record.Add(new IncomeAndPayRecordModel
-            {
-                Category = "支出",
-                Money = 16000,
-                //ImplementDate = DateTime.ParseExact("20160102", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
-                ImplementDate = new DateTime(2016, 1, 2)
-            });
-
-            record.Add(new IncomeAndPayRecordModel
-            {
-                Category = "支出",
-                Money = 8000,
-                //ImplementDate = DateTime.ParseExact("20160103", "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture)
-                ImplementDate = new DateTime(2016, 1, 3)
-            });
-
-            return View(record);
+                if(item.Categoryyy.Equals(1))
+                {
+                    viewModelList.Add(new IncomeAndPayRecordModel
+                    {
+                        Category = "支出",
+                        Money = item.Amounttt,
+                        ImplementDate = item.Dateee
+                    });
+                }
+                else
+                {
+                    viewModelList.Add(new IncomeAndPayRecordModel
+                    {
+                        Category = "收入",
+                        Money = item.Amounttt,
+                        ImplementDate = item.Dateee
+                    });
+                }
+            }
+            return View(viewModelList);
         }
     }
 }
